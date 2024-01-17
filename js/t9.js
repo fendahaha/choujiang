1d0836ef598121009779f795775cf895848c7db5
fendahaha <troneyf001@gmail.com>
2024-01-17 21:55:19 +0800

    ada

--- js/t9.js
+++ js/t9.js
@@ -37,6 +37,7 @@ const persons = [...Array(130).keys()].map(e => {
 });
 const persons_show = persons.slice(0, 17 * 7);
 const persons_hide = persons.slice(17 * 7);
+/**###############################*/
 const person_manager = {
     create_person: (e, hide = false) => {
         return `<div class="cj-person ${hide ? 'hide' : ''}" id="${e.id}">
@@ -51,7 +52,7 @@ const person_manager = {
     update_person: function (p_id, data) {
         $(`#${p_id}`).find(".cj-person-front-2").text(data.name);
         $(`#${p_id}`).find(".cj-person-front-3").text(data.employeeId);
-        $(`#${p_id}`).attr('id', data.id)
+        $(`#${p_id}`).attr('id', data.id);
     },
     render_show_person: function () {
         const persons_html = persons_show.map(e => person_manager.create_person(e)).join('');
@@ -68,7 +69,6 @@ const person_manager = {
         this.render_hide_person();
     }
 }
-
 const random_place_person = {
     random_place_person_animations: [],
     random_place_person_animations_finished: true,
@@ -126,17 +126,15 @@ const random_place_person = {
             Promise.all(
                 this.random_place_person_animations.map((animation) => animation.finished),
             ).then(() => {
-                _this.clear();
+                _this.random_place_person_animations.forEach(a => {
+                    a.commitStyles();
+                    a.cancel();
+                })
+                _this.random_place_person_animations = [];
+                _this.random_place_person_animations_finished = true;
             });
+            return this
         }
-    },
-    clear: function () {
-        this.random_place_person_animations.forEach(a => {
-            a.commitStyles();
-            a.cancel();
-        })
-        this.random_place_person_animations = [];
-        this.random_place_person_animations_finished = true;
     },
     finished: function () {
         return Promise.all(
@@ -144,106 +142,201 @@ const random_place_person = {
         )
     }
 }
-person_manager.render_person()
+const updating_manager = {
+    last_update_animation: null,
+    updating_interval_id: null,
+    is_updating: false,
+    loading: false,
+    update: function () {
+        let hide_index = Math.floor(Math.random() * persons_hide.length);
+        let show_index = Math.floor(Math.random() * persons_show.length);
+        let temp = persons_show[show_index];
+        persons_show[show_index] = persons_hide[hide_index];
+        persons_hide[hide_index] = temp;
+        person_manager.update_person(persons_hide[hide_index].id, persons_show[show_index])
+
+        const target = $(`#${persons_show[show_index].id} .cj-person-front`)[0];
+        const keyframes = [
+            {backgroundColor: `yellow`, offset: 0},
+            {backgroundColor: `rgba(0, 127, 127, ${(Math.random() * 6 + 2) / 10})`, offset: 1},
+        ];
+        const options = {
+            fill: "forwards",
+            duration: 1000,
+            iterations: 1,
+        };
+        const animation = target.animate(keyframes, options)
+        this.last_update_animation = animation;
+        animation.persist();
+    },
+    start_updating: function () {
+        if (!this.loading) {
+            this.loading = true;
+            if (this.is_updating) {
+                this.loading = false;
+                return Promise.resolve(undefined)
+            }
+            this.is_updating = true;
+            console.log('start');
+            const _this = this;
+            return random_place_person.finished().then(() => {
+                _this.updating_interval_id = setInterval(() => {
+                    _this.update();
+                }, 100);
+                _this.loading = false;
+            })
+        }
+    },
+    stop_updating: function () {
+        if (!this.loading) {
+            this.loading = true;
+            if (!this.is_updating) {
+                this.loading = false;
+                return Promise.resolve(undefined)
+            }
+            console.log('stop');
+            clearInterval(this.updating_interval_id);
+            const _this = this;
+            return this.last_update_animation.finished.then(() => {
+                _this.is_updating = false;
+                _this.loading = false;
+            })
+        }
+        return Promise.resolve(undefined)
+    }
+}
+
+person_manager.render_person();
 random_place_person.start();
-/**###########################*/
-let current_update_animation = null;
-const update = () => {
-    let hide_index = Math.floor(Math.random() * persons_hide.length);
-    let show_index = Math.floor(Math.random() * persons_show.length);
-    let $hide_target = $(`#${persons_hide[hide_index].id}`);
-    let $show_target = $(`#${persons_show[show_index].id}`);
-    let temp = persons_show[show_index];
-    persons_show[show_index] = persons_hide[hide_index];
-    persons_hide[hide_index] = temp;
-    let hide_clone = $hide_target.clone();
-    let show_clone = $show_target.clone();
-    $hide_target.replaceWith(show_clone);
-    $show_target.replaceWith(hide_clone);
-
-    const target = $(`#${persons_show[show_index].id} .cj-person-front`)[0];
-    const keyframes = [
-        // {backgroundColor: `blue`, offset: 0.1},
-        {backgroundColor: `yellow`, offset: 0},
-        {backgroundColor: `rgba(0, 127, 127, ${(Math.random() * 6 + 2) / 10})`, offset: 1},
-    ];
-    const options = {
-        fill: "forwards",
-        duration: 1000,
-        iterations: 1,
-    };
-    // target.animate(keyframes, options).persist()
-    const animation = target.animate(keyframes, options)
-    current_update_animation = animation;
-    animation.persist();
-    // animation.finished.then((animate) => {
-    //     try {
-    //         animate.commitStyles();
-    //         animate.cancel();
-    //     } catch (e) {
-    //         console.log('fenda', e);
-    //     }
-    // })
-}
-let updating_interval_id = null;
-const stop_updating = () => clearInterval(updating_interval_id);
-const start_updating = () => {
-    random_place_person.finished().then(() => {
-        updating_interval_id = setInterval(() => {
-            update()
-        }, 100)
-    })
-}
-
-start_updating();
+random_place_person.finished().then(() => {
+    updating_manager.start_updating();
+});
 
 /**#############################*/
-function getOffsets(_c) {
-    let c = document.getElementById(_c);
-    return {left: c.offsetLeft, top: c.offsetTop}
+const choose_manager = {
+    choose_data: {current: null},
+    last_animation: null,
+    choose_intervalId: null,
+    is_choosing: false,
+    loading: false,
+    getOffsets: (_c) => {
+        let c = document.getElementById(_c);
+        return {left: c.offsetLeft, top: c.offsetTop}
+    },
+    choose_next: function () {
+        const n = Math.floor(Math.random() * persons_show.length);
+        this.choose_data.current = n;
+        const offsets = this.getOffsets(persons_show[n].id);
+        const keyframes = [
+            {left: offsets.left + 'px', top: offsets.top + 'px'},
+        ];
+        const options = {
+            fill: "forwards",
+            easing: "ease-in",
+            duration: 100,
+            iterations: 1,
+        };
+        const animation = $("#cj-choose")[0].animate(keyframes, options);
+        this.last_animation = animation;
+        animation.persist();
+    },
+    start_choose: function () {
+        if (!this.loading) {
+            this.loading = true;
+            if (this.is_choosing) {
+                this.loading = false;
+                return
+            }
+            console.log("start choose");
+            $("#cj-choose").show();
+            const _this = this;
+            this.choose_intervalId = setInterval(() => _this.choose_next(), 100);
+            this.is_choosing = true;
+            this.loading = false;
+        }
+    },
+    stop_choose: function () {
+        if (!this.loading) {
+            this.loading = true;
+            if (!this.is_choosing) {
+                this.loading = false;
+                return Promise.resolve(undefined)
+            }
+            console.log("stop choose");
+            clearInterval(this.choose_intervalId);
+            const _this = this;
+            return this.last_animation.finished.then(() => {
+                console.log(_this.choose_data);
+                // $("#cj-choose").hide();
+                _this.is_choosing = false;
+                _this.loading = false;
+            })
+        } else {
+            return Promise.resolve(undefined)
+        }
+    },
 }
 
-const getRandowNum = () => Math.floor(Math.random() * persons_show.length);
-const next_data = {next: null};
-const choose_next = () => {
-    const n = getRandowNum();
-    next_data.next = n;
-    const offsets = getOffsets(persons_show[n].id);
-    $("#cj-choose").css(offsets)
-}
+// $("#test1").on('click', () => {
+//     choose_manager.start_choose()
+// })
+// $("#test2").on('click', () => {
+//     choose_manager.stop_choose()
+// })
 /**###############################*/
+
+// $("#go_to_lottery")
+// $("#start_lottery")
+// $("#stop_lottery")
+let loading = false;
+$("#go_to_lottery").on('click', () => {
+    if (!loading) {
+        loading = true;
+        $("#go_to_lottery").addClass('disable');
+        random_place_person.finished().then(() => {
+            updating_manager.stop_updating().then(() => {
+                $(".cj-container-show-persons").addClass("show_back")
+                $(".cj-container-show-persons .cj-person").css('transition', 'all 1s');
+                $(".cj-container-show-persons .cj-person").css('transform', 'translate3d(0, 0, 0) rotateY(180deg)');
+                setTimeout(() => {
+                    loading = false;
+                    $("#go_to_lottery").removeClass('disable');
+                    $("#go_to_lottery").hide();
+                    $("#start_lottery").show();
+                    $("#stop_lottery").hide();
+                }, 1000)
+            })
+        });
+    }
+});
+
+
 let is_lottery = false;
-let intervalId = null;
 const change_lottery_status = (is_lottery) => {
     if (is_lottery) {
         $("#start_lottery").hide();
         $("#stop_lottery").show();
 
-        stop_updating();
-        if (current_update_animation) {
-            current_update_animation.finished.then(() => {
-                $(".cj-container-show-persons").addClass("show_back")
-                $(".cj-container-show-persons .cj-person").css('transition', 'all 1s');
-                $(".cj-container-show-persons .cj-person").css('transform', 'translate3d(0, 0, 0) rotateY(180deg)');
-
-                setTimeout(() => {
-                    $("#cj-choose").show()
-                    intervalId = setInterval(() => choose_next(), 100)
-                }, 1100)
-            })
-        }
+        updating_manager.stop_updating().then(() => {
+            $(".cj-container-show-persons").addClass("show_back")
+            $(".cj-container-show-persons .cj-person").css('transition', 'all 1s');
+            $(".cj-container-show-persons .cj-person").css('transform', 'translate3d(0, 0, 0) rotateY(180deg)');
+            setTimeout(() => {
+                choose_manager.start_choose()
+            }, 1000)
+        })
     } else {
         $("#start_lottery").show();
         $("#stop_lottery").hide();
+        choose_manager.stop_choose().then(() => {
+            $(".cj-container-show-persons").removeClass("show_back")
+            $(".cj-container-show-persons .cj-person").css('transition', 'all 1s');
+            $(".cj-container-show-persons .cj-person").css('transform', 'translate3d(0, 0, 0) rotateY(0deg)');
+            setTimeout(() => {
+                updating_manager.start_updating();
+            }, 1000)
+        })
 
-        $("#cj-choose").hide()
-        clearInterval(intervalId)
-        $(".cj-container-show-persons").removeClass("show_back")
-        $(".cj-container-show-persons .cj-person").css('transition', 'all 1s');
-        $(".cj-container-show-persons .cj-person").css('transform', 'translate3d(0, 0, 0) rotateY(0deg)');
-        setTimeout(() => {
-            start_updating();
-        }, 1000)
     }
 }
 $("#start_lottery").on('click', () => {
